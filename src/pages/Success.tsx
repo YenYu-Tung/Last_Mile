f1a65582f37cd3e2748d71631b95eaa30ba4531a
YenYu-Tung <tungyy@LAPTOP-UTKHAJS5.localdomain>
2024-01-15 23:35:23 +0800

    feat: success layout

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -20,15 +20,13 @@ export default function Success({ setValue }: SuccessProps){
 
   return (
     <div className="bg-dark" style={{ height: "100vh" }}>
-      <div className="text-green text-3xl font-bold h-1/3 flex justify-center items-end pb-20">
-        Successfully !
-      </div>
-      <div className="flex flex-col items-center h-1/3">
-        <img className="w-24 sm:w-40" src={success} alt="Success" />
-        <span className="text-white text-xl font-bold mt-12">Please take package out</span>
-        <span className="text-light-gray text-md font-semibold w-3/5 text-center mt-6">Make sure the locker has closed before you leave</span>        
+      <div className="flex flex-col justify-end items-center h-3/5 lg:h-[65%]">
+        <span className="text-green text-3xl font-bold">Successfully !</span>
+        <img className="w-24 sm:w-32 mt-12" src={success} alt="Success" />
+        <span className="text-white text-2xl font-bold mt-12">Please take package out</span>
+        <span className="text-white text-md font-semibold w-3/5 text-center mt-6">Make sure the locker has closed before you leave</span>        
       </div>      
-      <div className="h-1/3 flex flex-col justify-center items-center">
+      <div className="h-2/5 lg:h-[35%] flex flex-col justify-center items-center">
         <button className="text-black text-xl font-bold py-5 rounded-[18px] bg-gradient-to-r from-[#7CFFC0] via-[#32FF9D] to-[#00ff85] w-3/4" onClick={handleNavigate}>
           Continue
         </button>
